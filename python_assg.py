1c70844e7d9b0769ac001a88d48cd607a0c56c06
GURACHAALI <151527288+GURACHAALI@users.noreply.github.com>
2025-03-25 09:41:00 +0000

    Add files via upload

---
+++ python_assg.py
@@ -0,0 +1,13 @@
+#the two numbers are in the variable x and y
+x=int(input("input no 1="))
+y=int(input("input no 2="))
+
+addition=x+y
+subtraction=x-y
+multiplication=x*y
+divission=x/y
+
+print('addition=',addition)
+print('subtraction=',subtraction)
+print('multiplication=',multiplication)
+print('divission=',divission)
